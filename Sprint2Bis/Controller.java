e82175a424885d30bf21b310efe8b95ce93c2862
Rafanomezantsoa1 <lalainarafanomezantsoaa@gmail.com>
2025-10-29 10:16:03 +0100

    Sprint 2 Bis annotation

---
+++ Sprint2Bis/Controller.java
@@ -0,0 +1,10 @@
+package classe;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME) // indispensable pour que ce soit visible à l'exécution
+public @interface Controller {
+
+}
+
